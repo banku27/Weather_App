a0d902e8492fa5482d32f12a699d68678071cf7b
banku27 <pankajparihar123321@gmail.com>
2022-05-20 22:41:01 +0530

    gwt selected city data

--- lib/ui/welcome_screen.dart
+++ lib/ui/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/city.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -10,6 +11,10 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
+    List<City> cities =
+        City.citiesList.where((city) => city.isDefault == false).toList();
+
+    List<City> selectedCities = City.getSelectedCities();
     return Scaffold();
   }
 }
